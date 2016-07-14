b7f144fb7584e2a82e9ba88a740084d3f06747c5
kolya770 <grinko.kolya770@gmail.com>
2016-07-14 11:22:44 +0300

    all blocks

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    var size_container = $('.size-container').height();
+    $('.left-block').css('height', size_container);
+    $('.right-block').css('height', size_container);
+});
