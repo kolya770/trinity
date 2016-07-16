0df7900886130e3776ed6743127f38cfdb717121
kolya770 <grinko.kolya770@gmail.com>
2016-07-16 20:11:34 +0300

    mobile v2 animation fade

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
+
+
 $(document).ready(function () {
-    var size_container = $('.size-container').height();
-    $('.left-block').css('height', size_container);
-    $('.right-block').css('height', size_container);
+
+    var md = new MobileDetect(window.navigator.userAgent);
+
+    if (!md.mobile()) {
+        var size_container = $('.size-container').height();
+        $('.left-block').css('height', size_container);
+        $('.right-block').css('height', size_container);
+
+        $('.fade-bottom').fadeThis({
+            speed: 1000
+        });
+
+    }
+
+
+    $("#lightgallery").lightGallery({
+        thumbnail:true,
+        mode: 'lg-slide-circular'
+    });
+
 });
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
